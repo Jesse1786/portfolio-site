f8dadbe13e9bc4b5e53925265cd3f09add8f287a
Jesse <jessezhang17@gmail.com>
2021-12-12 12:58:15 -0500

    First commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello! I'm Jesse,<br />
+        a full stack developer.
+      </SectionTitle>
+      <SectionText>
+        Currently completing my fourth year in Computer Science at the University of Toronto. I love building web applications.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
